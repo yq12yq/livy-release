bcc8714582849f5bb34d48fdcee30ab7c41e0499
Alex Bozarth <ajbozart@us.ibm.com>
2017-04-05 15:56:48 -0700

    LIVY-287. Add Deprecation to Livy Configurations and update naming. (#300)
    Updates to Livy configurations
    
    - Added config deprecation with alternatives to ClientConf, HTTPConf, RSCConf, and LivyConf.
    - Added framework for deprecation without alternatives when the need arises.
    - Updated naming conventions in code and templates to use - instead of _ or camelCase and deprecated previous configs.
    - Updated TestClientConf and added a new test.
    (cherry picked from commit 221aa9cf8f33b48fd9c792f67c61b78708bfae99)
    
    Signed-off-by: jerryshao <sshao@hortonworks.com>

--- integration-test/src/main/scala/com/cloudera/livy/test/framework/MiniCluster.scala
+++ integration-test/src/main/scala/com/cloudera/livy/test/framework/MiniCluster.scala
@@ -174,7 +174,7 @@ object MiniLivyMain extends MiniClusterBase {
     // server. Do it atomically since it's used by MiniCluster to detect when the Livy server
     // is up and ready.
     eventually(timeout(30 seconds), interval(1 second)) {
-      val serverUrlConf = Map("livy.server.serverUrl" -> server.serverUrl())
+      val serverUrlConf = Map("livy.server.server-url" -> server.serverUrl())
       saveProperties(serverUrlConf, new File(configPath + "/serverUrl.conf"))
     }
   }
@@ -295,7 +295,7 @@ class MiniCluster(config: Map[String, String]) extends Cluster with MiniClusterU
     val localLivy = start(MiniLivyMain.getClass, confFile, extraJavaArgs = jacocoArgs)
 
     val props = loadProperties(confFile)
-    livyUrl = props("livy.server.serverUrl")
+    livyUrl = props("livy.server.server-url")
 
     // Wait until Livy server responds.
     val httpClient = new AsyncHttpClient()
